d9f0e671ab00c8fc34aeef159a2904ca93944341
GodMadoka <xiaoyaofeirong@gmail.com>
2017-08-25 00:02:49 +0800

    New Solution

--- array/561. Array Partition I/R1.py
+++ array/561. Array Partition I/R1.py
@@ -15,6 +15,27 @@ def arrayPairSum(nums):
         for i in xrange(0,len(nums),2):
                res += nums[i]
         return res
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dic = {}
+        for i in nums:
+        	if i in dic:
+        		dic[i] += 1
+        	else:
+        		dic[i] = 1
+        res = 0
+        flag = True
+        for i in xrange(-10000,10001):
+        	if i in dic:
+        		for j in xrange(dic[i]):
+        			if flag:
+        				res += i
+        			flag = not flag
+        return res
 
 a = [1,4,3,2]
 print arrayPairSum(a)

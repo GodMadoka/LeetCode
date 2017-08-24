7c75c15c749d8788ff9e27c5c3c88e7e78d0fb7c
GodMadoka <xiaoyaofeirong@gmail.com>
2017-08-24 11:50:16 +0800

    New Solution

--- array/219. Contains Duplicate II/R1.py
+++ array/219. Contains Duplicate II/R1.py
@@ -6,20 +6,15 @@ Created on Wed May 17 16:10:59 2017
 """
 
 class Solution(object):
-    def containsNearbyDuplicate(self, nums, k):
+    def containsNearbyDuplicate(self, nums,k):
         """
         :type nums: List[int]
-        :type k: int
         :rtype: bool
         """
         dic = {}
         for i in xrange(len(nums)):
-               if nums[i] not in dic():
-                      dic[nums[i]] = i
-               else:
-                      if abs(dic[nums[i]] - i) <= k:
-                             return True
-                      dic[nums[i]] = i
+        	if nums[i] in dic and i - dic[nums[i]] <= k: return True
+        	else: dic[nums[i]] = i
         return False
                       
         
